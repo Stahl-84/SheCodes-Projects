57741b4ab8a83db6b9982881df8ff06a10faf50a
Stahl-84 <77590729+Stahl-84@users.noreply.github.com>
2022-10-18 19:08:50 +0200

    Added icons, wind and humidity

--- script.js
+++ script.js
@@ -42,11 +42,64 @@ if (minutes < 10) {
 h2.innerHTML = `${day} ${date} ${month} ${hours}:${minutes}`;
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "9b66c95801e4f42b76477efd2b4bf112";
   let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`;
-  console.log(apiUrl);
+
   axios.get(apiUrl).then(displayForecast);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  console.log(response.data);
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row"> `;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class=" col-2 cards">
+          <div class="forecast-date>${formatDay(
+            forecastDay.dt
+          )}></div>          
+              <div class="card">
+                <div class="cardHeader">
+                  <img src="http://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png" 
+                  class="card-img-top" alt="" />
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">${day}</h5>
+                  <p class="card-text">Sunny</p>
+                  <p class="temp">${forecastDay.temp}</p>
+                  <p class="card-text">Wind ${forecastDay.wind_speed}km/h</p>
+                  <p class="temp">Humidity ${forecastDay.humidity}%</p>
+
+                </div>
+              </div>
+            </div>`;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function displayName(response) {
@@ -139,41 +192,3 @@ let currentButton = document.querySelector("#current");
 currentButton.addEventListener("click", getCurrentPosition);
 
 search("Malaga");
-
-//FORECAST
-
-function displayForecast(response) {
-  console.log(response.data.daily);
-  let forecastElement = document.querySelector("#forecast");
-
-  let forecastHTML = `<div class="row"> `;
-  let days = [
-    //"Monday",
-    //"Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-  ];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class=" col-2 cards">
-              <div class="card">
-                <div class="cardHeader">
-                  <img src="media/sun.svg" class="card-img-top" alt="" />
-                </div>
-                <div class="card-body">
-                  <h5 class="card-title">${day}</h5>
-                  <p class="card-text">Sunny</p>
-                  <p class="temp">25&deg;</p>
-                  <p class="card-text">Wind 9km/h <br />Rain 0%</p>
-                </div>
-              </div>
-            </div>`;
-  });
-  forecastHTML = forecastHTML + `</div>`;
-
-  forecastElement.innerHTML = forecastHTML;
-}
