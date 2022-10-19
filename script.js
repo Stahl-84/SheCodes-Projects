22277e749cfb1ba23fcf9ad626aa732a5228967f
Stahl-84 <77590729+Stahl-84@users.noreply.github.com>
2022-10-19 08:59:36 +0200

    Added daily info + index

--- script.js
+++ script.js
@@ -43,7 +43,8 @@ h2.innerHTML = `${day} ${date} ${month} ${hours}:${minutes}`;
 
 function getForecast(coordinates) {
   let apiKey = "9b66c95801e4f42b76477efd2b4bf112";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude={part}&appid=${apiKey}&units=metric`;
+  // let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`;
 
   axios.get(apiUrl).then(displayForecast);
 }
@@ -66,18 +67,16 @@ function formatDay(timestamp) {
 
 function displayForecast(response) {
   let forecast = response.data.daily;
-  console.log(response.data);
+  console.log(response.data.daily);
+
   let forecastElement = document.querySelector("#forecast");
 
-  let forecastHTML = `<div class="row"> `;
+  let forecastHTML = `<div class="row cards"> `;
   forecast.forEach(function (forecastDay, index) {
     if (index < 6) {
       forecastHTML =
         forecastHTML +
-        `<div class=" col-2 cards">
-          <div class="forecast-date>${formatDay(
-            forecastDay.dt
-          )}></div>          
+        `<div class=" col-2 card">
               <div class="card">
                 <div class="cardHeader">
                   <img src="http://openweathermap.org/img/wn/${
@@ -86,19 +85,20 @@ function displayForecast(response) {
                   class="card-img-top" alt="" />
                 </div>
                 <div class="card-body">
-                  <h5 class="card-title">${day}</h5>
-                  <p class="card-text">Sunny</p>
-                  <p class="temp">${forecastDay.temp}</p>
-                  <p class="card-text">Wind ${forecastDay.wind_speed}km/h</p>
+                  <h5 class="card-title">${formatDay(forecastDay.dt)}</h5>
+                  <p class="card-text">${forecastDay.weather[0].description}</p>
+                  <p class="temp">${Math.round(forecastDay.temp.day)}&degC</p>
+                  <p class="card-text">Wind ${Math.round(
+                    forecastDay.wind_speed
+                  )}km/h</p>
                   <p class="temp">Humidity ${forecastDay.humidity}%</p>
-
                 </div>
               </div>
-            </div>`;
+      </div>`;
     }
   });
-  forecastHTML = forecastHTML + `</div>`;
 
+  forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
 
